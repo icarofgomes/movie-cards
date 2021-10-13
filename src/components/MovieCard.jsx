6964d9e7ef83c7486a0c54fcb744b3349852fd97
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-12 21:05:21 -0300

    Add Swipe and finished main screen

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,5 @@
+// https://larainfo.com/blogs/tailwind-css-carousel-slider-examples
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
@@ -6,22 +8,23 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, storyline, id, imagePath } = movie;
-    console.log(movie);
+    console.log(window.screen.width)
     return (
       <div
-        className="p-4 md:grid md:grid-cols-2 lg:grid-cols-3 gap-5 space-y-4 md:space-y-0"
+        className="p-4 md:p-2"
         data-testid="movie-card"
       >
-        <div className="max-w-sm bg-white px-6 pt-6 pb-2 rounded-xl shadow-lg transform
-          hover:scale-105 transition duration-500">
-          <h3 className="mb-3 text-xl font-bold text-indigo-600">{ title }</h3>
-          <div className="relative">
-            <img className="w-full rounded-xl" src={ imagePath } alt={ title } />
+        <div className="bg-white px-4 pt-4 pb-2 rounded-xl shadow-lg mx-auto">
+          <h3 className="mb-2 text-xl font-bold text-green-600">{ title }</h3>
+          <div className="">
+            <img className="max-h-80 md:max-h-48 mx-auto rounded-xl" src={ imagePath } alt={ title } />
           </div>
-          <p className="text-black">{ storyline }</p>
-          <div className="my-4">
-            <div className="mt-4 text-xl w-full text-white bg-indigo-600 py-1.5
-              rounded-xl shadow-lg text-center">
+          <div className="h-48 text-black text-sm md:pt-1 flex items-center">
+          <p className="">{ storyline }</p>
+          </div>
+          <div className="my-1">
+            <div className="mt-4 text-xl w-full text-white bg-green-600 py-1.5
+              rounded-xl shadow-lg text-center font-bold">
                 <Link to={ `/movies/${id}` }>VER DETALHES</Link>
             </div>
           </div>
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.min.css';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -26,15 +28,33 @@ class MovieList extends Component {
   render() {
     const { movies } = this.state;
     const movieList = movies.map(
-      (movie) => <MovieCard key={ movie.title } movie={ movie } />,
+      (movie) => (
+        <SwiperSlide>
+          <MovieCard key={ movie.title } movie={ movie }/>
+        </SwiperSlide>
+        )
     );
 
     if (movieList.length === 0) return <Loading />;
 
     return (
-      <div data-testid="movie-list">
-        { movieList }
-        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      <div
+        className="pb-2"
+        data-testid="movie-list">
+        <div className="h-5/6">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={ window.screen.width > 768 ? 3 : 1 }
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            { movieList }
+          </Swiper>
+        </div>
+        <div className="mx-auto text-xl w-2/3 text-white bg-blue-600 py-1.5
+              rounded-xl shadow-lg text-center font-bold md:w-2/6">
+          <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        </div>
       </div>
     );
   }
