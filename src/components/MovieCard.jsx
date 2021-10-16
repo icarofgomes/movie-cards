70e57a37190cf6fa3f6a663d2a804b5f0a795754
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-16 18:15:39 -0300

    Add Navigation

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 function MovieCard ({ movie }) {
-  const { title, storyline, id, imagePath } = movie;
+  const { title, id, rating, imagePath } = movie;
   return (
     <div
       className="p-4 md:p-2"
       data-testid="movie-card"
     >
-      <div className="bg-white px-4 pt-4 pb-2 rounded-xl shadow-lg mx-auto">
-        <h3 className="mb-2 text-xl font-bold text-green-600">{ title }</h3>
-        <div className="">
-          <img className="max-h-80 md:max-h-48 mx-auto rounded-xl" src={ imagePath } alt={ title } />
+      <div className="bg-white p-4 rounded-xl shadow-lg mx-auto">
+        <div className="flex justify-between pb-4">
+          <h3 className="text-xl font-bold text-green-600">{ title }</h3>
+          <div className="flex items-center justify-end">
+            <svg class="w-6 h-6 text-gray-900 fill-current" viewBox="0 0 24 24">
+              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
+            </svg>
+            <div className="text-green-600 font-bold text-xl ml-1">{ rating }</div>
+          </div>
         </div>
-        <div className="h-48 text-black text-sm md:pt-1 flex items-center">
-        <p className="">{ storyline }</p>
+        <div className="h-96 p-2">
+          <img className="max-h-96 md:max-h-80 mx-auto rounded-xl" src={ imagePath } alt={ title } />
         </div>
         <div className="my-1">
           <div className="mt-4 text-xl w-full text-white bg-green-600 py-1.5
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from 'react';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.min.css';
+import SwiperCore, { Navigation } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
 import * as movieAPI from '../services/movieAPI';
+
+SwiperCore.use([Navigation]);
 
 function MovieList () {
   const [movies, setMovies] = useState([]);
@@ -34,16 +38,19 @@ function MovieList () {
       data-testid="movie-list">
       <div className="h-5/6">
         <Swiper
+          navigation={ window.screen.width > 768 ? true : false }
           spaceBetween={50}
           slidesPerView={ window.screen.width > 768 ? 3 : 1 }
         >
           { movieList }
         </Swiper>
       </div>
-      <div className="mx-auto text-xl w-2/3 text-white bg-blue-600 py-1.5
-            rounded-xl shadow-lg text-center font-bold md:w-2/6">
-        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-      </div>
+      <Link to="/movies/new">
+        <div className="mx-auto text-xl w-2/3 text-white bg-blue-600 py-1.5
+          rounded-xl shadow-lg text-center font-bold md:w-2/6">
+          ADICIONAR CARTÃO
+        </div>
+      </Link>
     </div>
   );
 }
