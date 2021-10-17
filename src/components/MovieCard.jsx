43806f8f941326b5208f79fe4769a99e7deefe52
Icaro Ferreira <icaro.iky@gmail.com>
2021-10-16 21:10:53 -0300

    MovieList & MovieDetails Style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,8 @@ function MovieCard ({ movie }) {
           <h3 className="text-xl font-bold text-green-600">{ title }</h3>
           <div className="flex items-center justify-end">
             <svg class="w-6 h-6 text-gray-900 fill-current" viewBox="0 0 24 24">
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
+              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19
+                8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z"/>
             </svg>
             <div className="text-green-600 font-bold text-xl ml-1">{ rating }</div>
           </div>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -24,16 +24,54 @@ function MovieDetails ({ match: { params: { id } } }) {
   if (movie === '') return <Loading />;
 
   return (
-    <div data-testid="movie-details">
-      <h1>{ `Title: ${title}` }</h1>
-      <img alt="Movie Cover" src={ imagePath } width="250px"/>
-      <p>{ `Subtitle: ${subtitle}` }</p>
-      <p>{ `Storyline: ${storyline}` }</p>
-      <p>{ `Genre: ${genre}` }</p>
-      <p>{ `Rating: ${rating}` }</p>
-      <Link to="/movie-cards">VOLTAR</Link>
-      <Link to={ `${id}/edit` }>EDITAR</Link>
-      <Link to="/movie-cards" onClick={ deleteFunction }>DELETAR</Link>
+    <div className="bg-gray-800 py-2">
+      <div
+        className="flex flex-col bg-white w-11/12 mx-auto rounded-xl text-black p-4"
+        data-testid="movie-details"
+      >
+        <h1 className="text-green-600 font-bold text-xl mb-2 mx-auto">{ title }</h1>
+        <div className="md:flex">
+          <img className="max-h-80 rounded mx-auto mb-2"alt="Movie Cover" src={ imagePath }/>
+          <div className="md:p-4 md:flex md:flex-col md:justify-center">
+            <p className="text-lg md:my-2">
+              <span className="font-bold text-green-600 mr-1">Subtitle:</span>
+                { subtitle }
+              </p>
+            <p className="text-lg md:my-2">
+              <span className="text-green-600 font-bold mr-1">Storyline:</span>
+              { storyline }
+            </p>
+            <p className="text-lg md:my-2">
+              <span className="text-green-600 font-bold mr-1">Genre:</span>
+              { genre }
+            </p>
+            <p className="text-lg md:my-2">
+              <span className="text-green-600 font-bold mr-1">Rating:</span>
+              {rating}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <Link to="/movie-cards">
+            <div className="mt-4 px-1.5 text-xl w-full text-white bg-green-600 py-1.5
+              rounded-xl shadow-lg text-center font-bold">
+              VOLTAR
+            </div>
+          </Link>
+          <Link to={ `${id}/edit` }>
+            <div className="mt-4 px-1.5 text-xl w-full text-white bg-green-600 py-1.5
+              rounded-xl shadow-lg text-center font-bold">
+              EDITAR
+            </div>
+          </Link>
+          <Link to="/movie-cards" onClick={ deleteFunction }>
+            <div className="mt-4 px-1.5 text-xl w-full text-white bg-green-600 py-1.5
+              rounded-xl shadow-lg text-center font-bold">
+              DELETAR
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
